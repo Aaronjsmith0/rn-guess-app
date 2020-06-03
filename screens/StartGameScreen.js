d31904be8764eddc1bd9b6fd1a180f4c5e519644
Aaron Smith <aaron.smith@sgws.com>
2020-06-03 11:44:23 -0500

    finsihed up mostly

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,8 +3,11 @@ import { View, StyleSheet, Text, Button, TouchableWithoutFeedback, Keyboard, Ale
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
+import BodyText from '../components/BodyText';
 import Input from '../components/Input';
+import TitleText from '../components/TitleText';
 import NumberContainer from '../components/NumberContainer';
+import MainButton from '../components/MainButton';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -42,7 +45,9 @@ const StartGameScreen = props => {
             <Card style={styles.summaryContainer}>
                 <Text>You Selected</Text>
                 <NumberContainer>{selectedNumber}</NumberContainer>
-                <Button title='Start Game' onPress={() => props.onStartGame(selectedNumber)}/>
+                <MainButton title='Start Game' onPress={() => props.onStartGame(selectedNumber)}>
+                    START GAME!
+                </MainButton>
             </Card>
         );
     }
@@ -52,9 +57,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>  
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game</Text>
+                <TitleText style={styles.title}>Start a New Game</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input 
                         style={styles.input} 
                         blurOnSubmit 
@@ -83,7 +88,6 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     title: {
-        fontSize: 20,
         marginVertical: 10,
     },
     inputContainer: {
@@ -107,7 +111,7 @@ const styles = StyleSheet.create({
     summaryContainer: {
         marginTop: 20,
         alignItems: 'center'
-    }
+    },
 });
 
 export default StartGameScreen;
